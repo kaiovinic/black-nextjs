30149c83d5ed2200d4206eb10ae7fe4c926817b9
kaiovinic <kaiovini19@hotmail.com>
2024-01-16 00:17:53 -0300

    Página detalhes do produto

---
+++ src/pages/products/[id].tsx
@@ -0,0 +1,67 @@
+import Header from "@/components/Header";
+import ProductDetails from "@/components/ProductDetails";
+import {
+  ProductType,
+  fetchProduct,
+  fetchProducts,
+} from "@/components/services/products";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import Head from "next/head";
+
+import { ReactNode } from "react";
+import { Container } from "reactstrap";
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const id = context.params?.id;
+
+  if (typeof id === "string") {
+    const product = await fetchProduct(id);
+    return {
+      props: {
+        product,
+      },
+    };
+  }
+
+  return {
+    redirect: {
+      destination: "/products",
+      permanent: false,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const products = await fetchProducts();
+
+  const paths = products.map((product) => ({
+    params: { id: product.id.toString() },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+const Product: NextPage = (props: {
+  children?: ReactNode;
+  product?: ProductType;
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{props.product!.name}</title>
+        <meta name="description" content={props.product!.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <Container className="mt-5">
+        <ProductDetails product={props.product!} />
+      </Container>
+    </div>
+  );
+};
+
+export default Product;
